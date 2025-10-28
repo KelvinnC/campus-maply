9530136fe34cbe589da05fac8ec67e5db851009f
James Birnie <jkbirnie@gmail.com>
2025-10-27 17:09:57 -0700

    reverted main

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
-import Login from './Page/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+    <App/>
   </React.StrictMode>,
 )
 
