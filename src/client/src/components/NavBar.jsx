08e0ac43988da048b95a0797d4d049a4965603bc
petersza <petersza@student.ubc.ca>
2025-11-02 20:02:18 -0800

    add react router

--- src/client/src/components/NavBar.jsx
+++ src/client/src/components/NavBar.jsx
@@ -1,10 +1,20 @@
+import { Link, useLocation } from "react-router-dom"
 import "../css/navBar.css"
+
 export default function NavBar(){
+    const location = useLocation()
+    const isLoginPage = location.pathname === '/login'
+    
     return(
         <nav className="NavbarBox">
             <div className="navBarTitle">
                 Campus Maply
             </div>
+            {!isLoginPage && (
+                <Link to="/login" className="navBarLoginBtn">
+                    Login
+                </Link>
+            )}
         </nav>
     )
 }
